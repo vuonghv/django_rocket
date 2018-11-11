7cfaf00e2f06951f32d9e3f1cf5cae8ce61bd4cd
Vuong Hoang <vuonghv.cs@gmail.com>
2018-11-12 02:02:28 +0700

    Customize User model from AbstractUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(blank=True, null=True, unique=True, db_index=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        db_table = 'users'
--- django_rocket/settings.py
+++ django_rocket/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'django_rocket.urls'
+
+AUTH_USER_MODEL = 'users.User'
 
 TEMPLATES = [
     {
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'django_rocket.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-rocket-db',
+        'USER': 'django-rocket-user',
+        'PASSWORD': 'bottomsecret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
